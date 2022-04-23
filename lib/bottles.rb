327749c126d1dfb7d83de1e3c5c595e3f7a0c86a
Carl Gudmundsson <komoss@gmail.com>
2022-04-23 23:50:59 +0200

    Implement verses 99-0 with enumerable

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,6 +1,6 @@
 class Bottles
-  def verses(_, _)
-    verse(99) + "\n" + verse(98)
+  def verses(upper, lower)
+    upper.downto(lower).collect {|i| verse(i)}.join("\n")
   end
 
   def verse(number)
