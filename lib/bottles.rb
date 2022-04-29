5823c1a40280180ba4184717f72167202b117abc
Carl Gudmundsson <komoss@gmail.com>
2022-04-29 23:04:55 +0200

    Implement container method in else verse

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -20,8 +20,8 @@ class Bottles
       "Take #{pronoun(number)} down and pass it around, " +
       "#{quantity(number - 1)} #{container(number - 1)} of beer on the wall.\n"
     else
-      "#{number} bottles of beer on the wall, " +
-      "#{number} bottles of beer.\n" +
+      "#{number} #{container(number)} of beer on the wall, " +
+      "#{number} #{container(number)} of beer.\n" +
       "Take #{pronoun(number)} down and pass it around, " +
       "#{quantity(number - 1)} #{container(number - 1)} of beer on the wall.\n"
     end
