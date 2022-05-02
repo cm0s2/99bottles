ff5bb77deb5a707a38d3246b64b918e167ac0f61
Carl Gudmundsson <komoss@gmail.com>
2022-05-02 09:33:52 +0200

    Delete arguments and parameters from remaining extracted methods

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -19,19 +19,19 @@ class Bottles
   end
 
   def container(number)
-    BottleNumber.new(number).container(number)
+    BottleNumber.new(number).container
   end
 
   def action(number)
-    BottleNumber.new(number).action(number)
+    BottleNumber.new(number).action
   end
 
   def pronoun(number)
-    BottleNumber.new(number).pronoun(number)
+    BottleNumber.new(number).pronoun
   end
 
   def successor(number)
-    BottleNumber.new(number).successor(number)
+    BottleNumber.new(number).successor
   end
 end
 
@@ -41,7 +41,7 @@ class BottleNumber
     @number = number
   end
 
-  def container(number)
+  def container
     if number == 1
       "bottle"
     else
@@ -57,15 +57,15 @@ class BottleNumber
     end
   end
 
-  def action(number)
+  def action
     if number == 0
       "Go to the store and buy some more"
     else
-      "Take #{pronoun(number)} down and pass it around"
+      "Take #{pronoun} down and pass it around"
     end
   end
 
-  def pronoun(number)
+  def pronoun
     if number == 1
       "it"
     else
@@ -73,7 +73,7 @@ class BottleNumber
     end
   end
 
-  def successor(number)
+  def successor
     if number == 0
       99
     else
