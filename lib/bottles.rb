640c1200c94f9d1604f64725748ad4611581ae3d
Carl Gudmundsson <komoss@gmail.com>
2022-05-02 09:30:34 +0200

    Change other Bottles methods to call BottleNumber methods

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -15,39 +15,23 @@ class Bottles
   end
 
   def quantity(number)
-    BottleNumber.new(number).quantity(number)
+    BottleNumber.new(number).quantity
   end
 
   def container(number)
-    if number == 1
-      "bottle"
-    else
-      "bottles"
-    end
+    BottleNumber.new(number).container(number)
   end
 
   def action(number)
-    if number == 0
-      "Go to the store and buy some more"
-    else
-      "Take #{pronoun(number)} down and pass it around"
-    end
+    BottleNumber.new(number).action(number)
   end
 
   def pronoun(number)
-    if number == 1
-      "it"
-    else
-      "one"
-    end
+    BottleNumber.new(number).pronoun(number)
   end
 
   def successor(number)
-    if number == 0
-      99
-    else
-      number - 1
-    end
+    BottleNumber.new(number).successor(number)
   end
 end
 
@@ -65,7 +49,7 @@ class BottleNumber
     end
   end
 
-  def quantity(number)
+  def quantity
     if number == 0
       "no more"
     else
