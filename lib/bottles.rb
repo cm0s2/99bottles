b280ea932eed09e009cd3261182713827c8aafb4
Carl Gudmundsson <komoss@gmail.com>
2022-05-06 08:54:45 +0200

    Add and implement BottleNumber1

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -18,8 +18,11 @@ class Bottles
   end
 
   def bottle_number_for(number)
-    if number == 0
+    case number
+    when 0
       BottleNumber0
+    when 1
+      BottleNumber1
     else
       BottleNumber
     end.new(number)
@@ -37,11 +40,7 @@ class BottleNumber
   end
 
   def container
-    if number == 1
-      "bottle"
-    else
-      "bottles"
-    end
+    "bottles"
   end
 
   def quantity
@@ -53,11 +52,7 @@ class BottleNumber
   end
 
   def pronoun
-    if number == 1
-      "it"
-    else
-      "one"
-    end
+    "one"
   end
 
   def successor
@@ -76,5 +71,15 @@ class BottleNumber0 < BottleNumber
 
   def successor
     99
+  end
+end
+
+class BottleNumber1 < BottleNumber
+  def container
+    "bottle"
+  end
+
+  def pronoun
+    "it"
   end
 end
