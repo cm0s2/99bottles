46696ff257b80f356c95c033a7c2023da0789b86
Carl Gudmundsson <komoss@gmail.com>
2022-05-18 08:44:42 +0200

    Add BottleVerse tests

--- test/bottles_test.rb
+++ test/bottles_test.rb
@@ -3,13 +3,13 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require_relative '../lib/bottles'
 
-class BottlesTest < Minitest::Test
+class BottleVerseTest < Minitest::Test
   def test_the_first_verse
     expected = "99 bottles of beer on the wall, " +
       "99 bottles of beer.\n" +
       "Take one down and pass it around, " +
       "98 bottles of beer on the wall.\n"
-    assert_equal expected, Bottles.new.verse(99)
+    assert_equal expected, BottleVerse.lyrics(99)
   end
 
   def test_another_verse
@@ -17,7 +17,7 @@ class BottlesTest < Minitest::Test
       "3 bottles of beer.\n" +
       "Take one down and pass it around, " +
       "2 bottles of beer on the wall.\n"
-    assert_equal expected, Bottles.new.verse(3)
+    assert_equal expected, BottleVerse.lyrics(3)
   end
 
   def test_verse_2
@@ -25,7 +25,7 @@ class BottlesTest < Minitest::Test
       "2 bottles of beer.\n" +
       "Take one down and pass it around, " +
       "1 bottle of beer on the wall.\n"
-    assert_equal expected, Bottles.new.verse(2)
+    assert_equal expected, BottleVerse.lyrics(2)
   end
 
   def test_verse_1
@@ -33,7 +33,7 @@ class BottlesTest < Minitest::Test
       "1 bottle of beer.\n" +
       "Take it down and pass it around, " +
       "no more bottles of beer on the wall.\n"
-    assert_equal expected, Bottles.new.verse(1)
+    assert_equal expected, BottleVerse.lyrics(1)
   end
 
   def test_verse_0
@@ -41,9 +41,11 @@ class BottlesTest < Minitest::Test
       "no more bottles of beer.\n" +
       "Go to the store and buy some more, " +
       "99 bottles of beer on the wall.\n"
-    assert_equal expected, Bottles.new.verse(0)
+    assert_equal expected, BottleVerse.lyrics(0)
   end
+end
 
+class BottlesTest < Minitest::Test
   def test_a_couple_verses
     expected = "99 bottles of beer on the wall, " +
       "99 bottles of beer.\n" +
