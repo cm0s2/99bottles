6e7d683c348b04f18ce7875011186ed48cf1561a
Carl Gudmundsson <komoss@gmail.com>
2022-05-18 10:57:34 +0200

    Add role player test for VerseFake

--- test/bottles_test.rb
+++ test/bottles_test.rb
@@ -79,6 +79,14 @@ class VerseFake
   end
 end
 
+class VerseFakeTest < Minitest::Test
+  include VerseRoleTest
+
+  def setup
+    @role_player = BottleVerse
+  end
+end
+
 class CountdownSongTest < Minitest::Test
   def test_verse
     expected = "This is verse 500.\n"
